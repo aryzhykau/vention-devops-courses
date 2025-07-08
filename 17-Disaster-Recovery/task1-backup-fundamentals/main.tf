8c90761a93f9717a98008ee0d56087b71fae56eb
Peter Tatransky <peto.tatransky@gmail.com>
2025-07-08 22:58:41 +0200

    Add Load Balancer module and secure EC2 with ALB security group

--- 17-Disaster-Recovery/task1-backup-fundamentals/main.tf
+++ 17-Disaster-Recovery/task1-backup-fundamentals/main.tf
@@ -12,15 +12,16 @@ module "ec2" {
   ami           = "ami-03cceb19490f64e93"  
   instance_type = "t2.micro"
   subnet_id     = module.vpc.subnet_ids[0]
+  vpc_id        = module.vpc.vpc_id            
+  alb_sg_id     = aws_security_group.alb_sg.id 
 }
-
 
 module "rds" {
   source         = "./modules/rds"
   instance_class = "db.t3.micro"
   username       = var.rds_username
   password       = var.rds_password
-  subnet_ids     = module.vpc.subnet_ids     # Pass both subnets
+  subnet_ids     = module.vpc.subnet_ids
 }
 
 module "s3" {
@@ -32,4 +33,39 @@ module "cloudfront" {
   source    = "./modules/cloudfront"
   s3_domain = "${module.s3.bucket_name}.s3.amazonaws.com"
 }
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+module "loadbalancer" {
+  source            = "./modules/loadbalancer"
+  subnet_ids        = module.vpc.subnet_ids
+  security_group_id = aws_security_group.alb_sg.id
+  vpc_id            = module.vpc.vpc_id
+  instance_id       = module.ec2.instance_id
+  environment       = "dev"
+}
+
+
 
---
+++ 17-Disaster-Recovery/task1-backup-fundamentals/modules/loadbalancer/main.tf
@@ -0,0 +1,53 @@
+resource "aws_lb" "app_alb" {
+  name               = "app-loadbalancer"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.subnet_ids
+  security_groups    = [var.security_group_id] # Passed in from root module
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${var.environment}-app-alb"
+  }
+}
+
+resource "aws_lb_target_group" "app_tg" {
+  name        = "${var.environment}-app-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "instance"
+
+  health_check {
+    path                = "/api/health"  
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "${var.environment}-app-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "ec2_attach" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = var.instance_id
+  port             = 80
+}
+
+
